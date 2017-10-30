c738dfb77dccc58ef1de0aa72b0c24c6fd28a54a
Milorad Kubatovic <m.kubatovic@gmail.com>
2017-10-30 17:54:21 +0100

    Docker

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 case $1 in
     "local") docker build -f Dockerfile_local -t localimage . ;;
-    "remote") docker build -f Dockerfile_remote -t remoteimage --build-arg ssh_prv_key="$(cat id_rsa1)" --build-arg ssh_pub_key="$(cat id_rsa1.pub)"  . ;;
+    "remote") docker build -f Dockerfile_remote -t remoteimage --build-arg ssh_prv_key="$(cat id_rsa)" --build-arg ssh_pub_key="$(cat id_rsa.pub)"  . ;;
     "default") docker build -f Dockerfile_local -t localimage . ;;
 esac
